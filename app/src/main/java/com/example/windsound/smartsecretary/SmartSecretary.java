c3ce1bc0ef74b47249b2329ddf90fc99f4434585
Unknown <rmp4m3jun@gmail.com>
2018-08-02 15:41:00 +0800

    Initial import
    Initial import

---
+++ app/src/main/java/com/example/windsound/smartsecretary/SmartSecretary.java
@@ -0,0 +1,13 @@
+package com.example.windsound.smartsecretary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmartSecretary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart_secretary);
+    }
+}
