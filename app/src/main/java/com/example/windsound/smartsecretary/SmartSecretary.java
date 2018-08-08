13398b013c46b12cfe51bbc49e1b0e843f3147a3
CHUN-YU <rmp4m3jun@gmail.com>
2018-08-08 17:56:48 +0800

    add .class
    add .class

--- app/src/main/java/com/example/windsound/smartsecretary/SmartSecretary.java
+++ app/src/main/java/com/example/windsound/smartsecretary/SmartSecretary.java
@@ -1,13 +1,35 @@
 package com.example.windsound.smartsecretary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SmartSecretary extends AppCompatActivity {
-
+    private Button new_button;
+    private Button my_alarm_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smart_secretary);
+        new_button = (Button) findViewById(R.id.new_button);
+        new_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(SmartSecretary.this, AddItem.class);
+                startActivity(intent);
+            }
+        });
+        my_alarm_button = (Button) findViewById(R.id.my_alarm_button);
+        my_alarm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(SmartSecretary.this, Alarm.class);
+                startActivity(intent);
+            }
+        });
     }
 }
